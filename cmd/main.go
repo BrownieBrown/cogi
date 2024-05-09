ff36d34591589da840e1afa48aaa7a5914a268a0
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2024-05-09 16:38:07 +0200

    Add main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
-package cmd
+package main
+
+import (
+	"github.com/BrownieBrown/cogi.git/internal/config"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := config.LoadConfig()
+	log.Println("Starting server...")
+
+	log.Println("Server started on port", cfg.API.Port)
+
+	err = http.ListenAndServe(cfg.API.Port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
