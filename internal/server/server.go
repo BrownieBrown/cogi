6a937de7a6dab12652f34266ae0252adec4055aa
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2024-05-09 16:37:30 +0200

    Add http server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,15 @@
 package server
+
+import (
+	"log"
+	"net/http"
+)
+
+func StartServer(config Config) {
+	log.Println("Starting server...")
+
+	err := http.ListenAndServe(config.Port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
